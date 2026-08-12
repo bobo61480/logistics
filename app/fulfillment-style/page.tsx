0b6877ccdba39cc16c5b1678cd354afc62204625
bobo61480 <alex@stylekoreanus.com>
2026-08-11 23:47:42 -0700

    refactor: reuse shared style switcher on fulfillment route

--- app/fulfillment-style/page.tsx
+++ app/fulfillment-style/page.tsx
@@ -3,20 +3,9 @@
 import Home from "../page";
 import styles from "../style-variants.module.css";
 
-function VariantNav() {
-  return (
-    <nav className={styles.variantNav} aria-label="Dashboard style variants">
-      <a href="/">Original</a>
-      <a href="/light">Light Dashboard</a>
-      <a href="/fulfillment-style">Fulfillment Style</a>
-    </nav>
-  );
-}
-
 export default function FulfillmentStyleDashboardPage() {
   return (
     <div className={`${styles.variantPage} ${styles.fulfillment}`}>
-      <VariantNav />
       <Home />
     </div>
   );
